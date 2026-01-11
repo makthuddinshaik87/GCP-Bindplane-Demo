f1b83c07d6f2135911e3ad7ecf4a4331a28274be
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 16:47:56 +0530

    Update variables.tf

--- terraform/part2-control-plane/variables.tf
+++ terraform/part2-control-plane/variables.tf
@@ -1,28 +1,19 @@
 
-variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "GCP Region"
-  type        = string
-}
+variable "project_id" { type = string }
+variable "region"     { type = string }
 
 variable "db_password" {
-  description = "Cloud SQL PostgreSQL password"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "admin_password" {
-  description = "BindPlane admin password"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "db_admin" {
-  description = "Database admin user (e.g., postgres)"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
+``
