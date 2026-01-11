11957b7854c2b9de666c072ef2a7233f3f4d2821
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 13:47:22 +0530

    Update variables.tf

--- terraform/part1-db/variables.tf
+++ terraform/part1-db/variables.tf
@@ -1,17 +1,12 @@
 variable "project_id" {
   type = string
 }
-
+ 
 variable "region" {
   type = string
 }
-
+ 
 variable "db_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "admin_password" {
   type      = string
   sensitive = true
 }
